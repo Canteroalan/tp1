96c27a8f17079b76f0e82762fd22cfdde4836603
Cantero Alan <cantero.alan456@gmail.com>
2020-07-22 19:53:26 -0300

    hice iterativo lo de nacho

--- tp1.c
+++ tp1.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-
-int main (){
+int main (int argc,char * argv[]){
+	if(argc != 2){
+		fprintf(stderr,"funciono con dos argumentos\n");
+		return 1;
+	}
+	printf("%s\n",argv[1]);
 	return 0;
 }
