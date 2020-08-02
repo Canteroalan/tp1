908fa48516cffd4141188dc4edb03d843a85086d
ignaciolazzaro <ilazzaro@fi.uba.ar>
2020-08-02 00:07:06 -0300

    tiene errores con valgrind, pero funciona (CASI TERMINADO) - sintetizador.c

--- sintetizador.c
+++ sintetizador.c
@@ -9,21 +9,75 @@
 
 #define MAX 256
 
+
+typedef struct {
+	int *frecuencia;
+	float *intensidad;
+	int cantidad_armonicos;
+	char *func_mod[3];
+	float parametros[3][3];
+} dato_t;
+
+
+dato_t *crear_dato_t(FILE *s);
+void destruir_dato_t(dato_t *d);
 char **split(const char * s, const int * anchos, size_t ncampos);
 void destruir_lineas(char **lineas, size_t n);
 void imprimir_lineas(char **ss, size_t n);
+bool leer_sintetizador(FILE *s, int n, int *f, float *a, char *func_mod[3], float parametros[3][3]);
+bool leer_func_mod(char *s, char *func_mod, float parametros[3]);
 
 
+dato_t *crear_dato_t(FILE *s){
+
+	dato_t *d = malloc(sizeof(dato_t));
+	if(d == NULL)
+		return NULL;
+
+	char *q = malloc(sizeof(char) * MAX);
+	if(q == NULL)
+		return false;
+
+	d->cantidad_armonicos = atoi(fgets(q, MAX, s));
+
+	d->frecuencia = malloc(d->cantidad_armonicos * sizeof(int));
+	if(d->frecuencia == NULL)
+		return NULL;
+
+	d->intensidad = malloc(d->cantidad_armonicos * sizeof(float));
+	if(d->intensidad == NULL)
+		return NULL;
+
+
+	for(size_t i = 0; i < 3; i++){
+		
+		d->func_mod[i] = malloc(sizeof(char) * 15);
+		if(d->func_mod[i] == NULL)
+			return NULL;
+	}
+
+	free(q);
+
+	return d;
+}
+
+void destruir_dato_t(dato_t *d){
+	free(d->frecuencia);
+	free(d->intensidad);
+
+	for(size_t i = 0; i < 3; i++)
+		free(d->func_mod[i]);
+
+	free(d);
+}
 
 char **split(const char *s, const int *anchos, size_t ncampos){
 
     size_t recorrido = 0;
 
     char **ss = malloc(sizeof(char *) * ncampos);
-    if(ss == NULL){
-    	free(ss);
+    if(ss == NULL)
         return NULL;
-    }
 
     for (size_t i = 0; i < ncampos; i++) {
         
@@ -41,7 +95,6 @@ char **split(const char *s, const int *anchos, size_t ncampos){
     return ss;
 }
 
-
 void destruir_lineas(char **lineas, size_t n){
     for(size_t i = 0; i < n; i++){
         free(lineas[i]);
@@ -50,54 +103,91 @@ void destruir_lineas(char **lineas, size_t n){
     free(lineas);
 }
 
-
-
-bool leer_sintetizador(FILE *s, int n, int *f, float *a){
+bool leer_sintetizador(FILE *s, int n, int *f, float *a, char *func_mod[3], float parametros[3][3]){
 	
 	char *q = malloc(sizeof(char) * MAX);
-	if(q == NULL){
-		free(q);
+	if(q == NULL)
 		return false;
-	}
 
 	int anchos_fa[3] = {1, 1, 8};
 
-    char **ss = malloc(sizeof(char *) * MAX);
-    if(ss == NULL){
-    	free(ss);
-    	free(q);
-        return false;
-    }
-
-	//n = atoi(fgets(q, MAX, s));
-	//printf("%d\n", n);
-
-	int mult = 10;
+	//int mult = 10;
 
 	for(size_t i = 0; i < n; i++){
 		fgets(q, MAX, s);
 		
-		if((f[i - 1] + 1) / mult != 0){
+		//En caso de que la frecuencia supere 1 cifra.
+		/*if(((f[i - 1] + 1) / mult) != 0){
 			anchos_fa[0]++;
 			mult *= 10;
-		}
+		}*/
 		
-		ss = split(q, anchos_fa, 3);
+		char **ss = split(q, anchos_fa, 3);
+		if(ss == NULL){
+    		free(q);
+        	return false;
+    	}
+
 		f[i] = atoi(ss[0]);
 		a[i] = atof(ss[2]);
-		//printf("%d %f\n", f[i], a[i]);
+
+		destruir_lineas(ss, 3);
 	}
 
-
-	/*for(size_t i = 0; i < 3; i++){
+	for(size_t i = 0; i < 3; i++){
 		fgets(q, MAX, s);
-		printf("%s", q);
-	}*/
+		leer_func_mod(q, func_mod[i], parametros[i]);
+	}
 
 	free(q);
-	destruir_lineas(ss, 3);
-	
+
 	return true;
+}
+
+bool leer_func_mod(char *s, char *func_mod, float parametros[3]){
+	int contador = 1;
+    
+    for(size_t i = 0; s[i] != '\n'; i++)
+        if(s[i] == ' ')
+        	contador++;
+
+    if(contador == 1){
+    	for(size_t j = 0; s[j] != '\n'; j++)
+    			func_mod[j] = s[j];
+
+    	for(size_t i = 0; i < 3; i++)
+    		parametros[i] = 0;
+
+    	return true;
+    }
+
+    char vector[8];
+
+    size_t n = 0;
+
+    for(size_t i = 0; i < (contador - 1); i++){
+
+    	if(i == 0){
+    		for(size_t j = 0; s[j] != ' '; j++)
+    			func_mod[j] = s[j];
+
+    		n += strlen(func_mod) + 1;
+    	}
+
+    	for(size_t i = 0; i < 8; i++)
+    		vector[i] = 0;
+
+    	size_t j;
+
+	    for(j = 0; (s[(j + n)] != ' ') && (s[(j + n)] != '\n'); j++)
+		    vector[j] = s[j + n];
+
+		parametros[i] = atof(vector);
+
+        n += (j + 1);
+    }
+
+    return true;
 }
 
 
@@ -109,49 +199,35 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	char *q = malloc(sizeof(char) * MAX);
-	if(q == NULL){
-		free(q);
+	dato_t *dato = crear_dato_t(s);
+	if(dato == NULL){
 		fclose(s);
 		return 1;
 	}
 
-	int n = atoi(fgets(q, MAX, s));
-	
-	int *f = malloc(sizeof(int) * n);
-	if(f == NULL){
-		free(f);
-		free(q);
+	if(! leer_sintetizador(s, dato->cantidad_armonicos, dato->frecuencia, dato->intensidad, dato->func_mod, dato->parametros)){
+		destruir_dato_t(dato);
 		fclose(s);
 		return 1;
 	}
 
-	float *a = malloc(sizeof(float) * n);
-	if(a == NULL){
-		free(f);
-		free(a);
-		free(q);
-		fclose(s);
-		return 1;
+	printf("%d\n", dato->cantidad_armonicos);
+
+	for(size_t i = 0; i < dato->cantidad_armonicos; i++)
+		printf("%d %f\n", dato->frecuencia[i], dato->intensidad[i]);
+
+	for(size_t i = 0; i < 3; i++){
+		printf("%s ", dato->func_mod[i]);
+
+		for(size_t j = 0; j < 3; j++)
+			printf("%f ", dato->parametros[i][j]);
+
+		printf("\n");
 	}
 
-	if(! leer_sintetizador(s, n, f, a)){
-		fclose(s);
-		free(a);
-		free(f);
-		free(q);
-		return 1;
-	}
 
-	printf("%d\n", n);
-
-	for(size_t i = 0; i < n; i++)
-		printf("%d %f\n", f[i], a[i]);
-
+	destruir_dato_t(dato);
 	fclose(s);
-	free(f);
-	free(a);
-	free(q);
 
 	return 0;
 }
